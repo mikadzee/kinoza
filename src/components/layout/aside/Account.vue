ddcf35c6d410b35cd7ba66a3d5b271dcc9759efa
Mikadze <asansabatyrov@gmail.com>
2026-09-06 15:05:28 +0300

    fix: account styles

--- src/components/layout/aside/Account.vue
+++ src/components/layout/aside/Account.vue
@@ -1,10 +1,21 @@
 <template>
   <div class="account-app">
-    <RouterLink to="/account">
+    <RouterLink to="/account" v-if="store?.dataUser?.email">
       <span>A</span>
+    </RouterLink>
+    <RouterLink to="/auth" v-else class="no-account">
+      <span>Войти</span>
     </RouterLink>
   </div>
 </template>
+
+<script setup>
+  import { useAuthStore} from "@/stores/auth.ts";
+
+  const store = useAuthStore();
+
+
+</script>
 
 <style lang="less">
 @import "./account.less";
